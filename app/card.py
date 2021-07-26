e7b1f543cf53ad5a51ff8db97a889e340aad494d
Sigmanificient <edhyjox@gmail.com>
2021-07-26 23:04:06 +0200

    Exporting main.py to app folder, created core class

---
+++ app/card.py
@@ -0,0 +1,48 @@
+from app.contstants import WIN_WIDTH, WIN_HEIGHT
+from random import randint
+
+import pygame
+
+
+class Card:
+    sheet = pygame.image.load("img/cards.png").convert()
+    w, h = (71, 96)
+
+    def __init__(self):
+        """Initialize a Card to be moved on the board."""
+        self.sprite = pygame.Surface((Card.w, Card.h))
+        self.rect = self.sprite.get_rect()
+
+        offset_rect = pygame.Rect(
+            Card.w * randint(0, 12), Card.h * randint(0, 3), Card.w, Card.h
+        )
+
+        self.sprite.blit(Card.sheet, self.rect, offset_rect)
+
+        self.rect.x = WIN_WIDTH if randint(0, 1) else -Card.w
+        self.rect.y = randint(-WIN_HEIGHT, WIN_HEIGHT)
+        self.invincible_cycles = 10
+
+        self.y_momentum = 0
+        self.direction = (1 if randint(0, 1) else -1) * randint(1, 3)
+        self.gain = 0.2
+
+    def move(self, screen_rect):
+        if self.rect.y > WIN_HEIGHT - Card.h:
+            self.y_momentum = -self.y_momentum
+            self.gain *= randint(1, 2)
+        else:
+            self.y_momentum += self.gain
+
+        self.rect.x += self.direction
+        self.rect.y += self.y_momentum
+
+        if self.invincible_cycles:
+            self.invincible_cycles -= 1
+            return
+
+        if not self.rect.colliderect(screen_rect) or self.gain > 50:
+            self.__init__()
+
+    def draw(self, screen):
+        screen.blit(self.sprite, self.rect)
---
+++ app/core.py
@@ -0,0 +1,43 @@
+from app.contstants import WIN_WIDTH, WIN_HEIGHT
+from app.card import Card
+import pygame
+
+
+class App:
+
+    def __init__(self):
+        self.is_running = False
+        self.cards = [Card() for _ in range(100)]
+
+        self.clock = pygame.time.Clock()
+
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.screen_rect = self.screen.get_rect()
+
+        icon = pygame.Surface((32, 32), pygame.SRCALPHA)
+        pygame.display.set_icon(icon)
+
+    def run(self):
+        self.is_running = True
+        self.screen.fill((0, 99, 0))
+
+        while self.is_running:
+            pygame.display.set_caption(
+                f"Solitaire XP Win - {self.clock.get_fps():.0f} Fps"
+            )
+
+            for card in self.cards:
+                card.move(self.screen_rect)
+                card.draw(self.screen)
+
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.is_running = False
+
+            self.clock.tick(60)
+
+    def __del__(self):
+        pygame.display.quit()
+        pygame.quit()
